03eb5c26ef14eefc3ebb3d9a1c1f1e3a278cc502
Avninder Preet Singh <53931646+Avninder99@users.noreply.github.com>
2021-06-11 18:13:22 +0530

    Added department_seed.js and event_seed.js files for seeding database

---
+++ seeds/event_seed.js
@@ -0,0 +1,47 @@
+const mongoose  = require("mongoose"),
+      Event     = require("../models/event"),
+      events    = require("./event_seed.json");
+
+const DBaddress =
+    process.env.DB_URL || "mongodb://localhost:27017/ecell-website";
+
+/*
+    run "node ./seeds/event_seed.js"
+    to seed the departments collection
+*/
+
+mongoose.connect(DBaddress, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+(async () => {
+    await Event.deleteMany({}, (err, deletedEvents) => {
+        if (err) {
+            console.log(err);
+            console.log(
+                "An error occured while dropping the events collection"
+            );
+            return process.exit(0);
+        } else {
+            console.log(deletedEvents);
+            console.log("Events collection has been cleared");
+            (async () => {
+                await Event.insertMany(events, (err, seededEvents) => {
+                    if (err) {
+                        console.log(err);
+                        console.log(
+                            "An error occured while seeding the events collection"
+                        );
+                        return process.exit(0);
+                    } else {
+                        console.log("Events collection has been seeded");
+                        return process.exit(0);
+                    }
+                });
+            })();
+        }
+    });
+})();
