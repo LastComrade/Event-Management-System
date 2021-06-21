56853812a199513accde1f39f0815f59ff1c7ef8
Konark Lohat <konarklohat123456@gmail.com>
2021-06-21 12:00:13 +0530

    DB Url

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const corsOptions = {
 };
 
 // Getting DB Url from the .env file of the local DB
-const DBurl = process.env.DB_UR || "mongodb://localhost:27017/ecell-website";
+const DBurl = process.env.DB_URL || "mongodb://localhost:27017/ecell-website";
 
 // Creating a mongoose connection
 mongoose.connect(DBurl, {
