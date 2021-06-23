0406916c7c2ffaeb8386e276ca23ff59cd3e7c2d
Konark Lohat <konarklohat123456@gmail.com>
2021-06-23 23:26:10 +0530

    DB URL Typo

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const corsOptions = {
 };
 
 // Getting DB Url from the .env file of the local DB
-const { DB_URL = "mongodb://localhost:27017/ecell-website" } = process.env;
+const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/ecell-website" ;
 
 // Creating a mongoose connection
 mongoose.connect(DB_URL, {
