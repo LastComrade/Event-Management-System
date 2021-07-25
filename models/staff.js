c7903ae6defb6a70afc07790c1eba423bd37be4d
Avninder Preet Singh <53931646+Avninder99@users.noreply.github.com>
2021-07-25 16:25:44 +0530

    1. Updated staff registration 2. Established reference relation between departments and staff members 3. Updated department and staff schemas

--- models/staff.js
+++ models/staff.js
@@ -7,77 +7,91 @@ const bcrypt = require("bcrypt");
 // defines that no username in the DB can be same
 
 const staffSchema = new Schema(
-    {
-        firstname: {
-            type: String,
-            required: true,
-        },
-        lastname: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-
-        // Department will be verified in the registration middleware
-        department: {
-            type: String,
-            required: true,
-        },
-        designation: {
-            type: String,
-            default: "Active Member",
-        },
-        profile_pic_url: {
-            type: String,
-            default: process.env.PROFILE_PIC_URL,
-        },
-        key: {
-            type: String,
-            required: true,
-        },
-        accActive: {
-            type: Boolean,
-            default: true,
-        },
-        resetPasswordLink: {
-            type: String,
-            default: "",
-        },
-        registerPasswordToken: {
-            type: String,
-            default: "",
-        },
-        role: {
-            type: String,
-            default: "member",
-        },
+  {
+    firstname: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+
+    // Department will be verified in the registration middleware
+    department: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Department",
+      },
+    ],
+    designation: {
+      type: String,
+      default: "Active Member",
+    },
+    sl_li: {
+      type: String,
+      default: "https://www.linkedin.com/company/ecellgndec/"
+    },
+    sl_ig: {
+      type: String,
+      default: "https://www.instagram.com/ecell.gndec/"
+    },
+    sl_fb: {
+      type: String,
+      default: "https://www.facebook.com/E-Cell-GNDEC-100549411816777/"
+    },
+    profile_pic_url: {
+      type: String,
+      default: process.env.PROFILE_PIC_URL,
+    },
+    key: {
+      type: String,
+      required: true,
+    },
+    accActive: {
+      type: Boolean,
+      default: true,
+    },
+    resetPasswordLink: {
+      type: String,
+      default: "",
+    },
+    registerPasswordToken: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: String,
+      default: "member",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 staffSchema.virtual("fullname").get(function () {
-    return this.firstname + " " + this.lastname;
+  return this.firstname + " " + this.lastname;
 });
 
 staffSchema.pre("save", async function (next) {
-    try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(this.password, salt);
-        this.password = hashedPassword;
-        next();
-    } catch (err) {
-        next(ErrorHandler.serverError());
-    }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (err) {
+    next(ErrorHandler.serverError());
+  }
 });
 
 module.exports = mongoose.model("Staff", staffSchema);
