90c054c3b17263692e2be8cce5955c8b5d0756d8
Avninder Preet Singh <53931646+Avninder99@users.noreply.github.com>
2021-07-28 08:10:56 +0530

    1. Added four level auth middlewares 2. minor changes in contact-form.ejs

--- middleware/auth.js
+++ middleware/auth.js
@@ -21,55 +21,249 @@ const authMid = {
   },
 
   staffCheck: (req, res, next) => {
-    const token = req.cookies.jwt_token;
-    if (token) {
-      jwt.verify(
-        token,
-        process.env.JWT_SECRET,
-        async (err, decodedToken) => {
-          if (err) {
-            res.locals.staff = null;
-            next();
-          } else {
-            const staff = await Staff.findOne(
-              {
-                _id: decodedToken.id,
-              },
-              (err, staff) => {
+    try{
+      const token = req.cookies.jwt_token;
+      if (token) {
+        jwt.verify(
+          token,
+          process.env.JWT_SECRET,
+          async (err, decodedToken) => {
+            if (err) {
+              res.locals.staff = null;
+              next();
+            } else {
+              const staff = await Staff.findOne(
+                {
+                  _id: decodedToken.id,
+                },
+                (err, staff) => {
+                  if (err) {
+                    console.log(err);
+                    next(ErrorHandler.serverError());
+                  } else if (staff) {
+                    const { id, fullname, designation, role, profile_pic_url } = staff;
+                    const staffData = { id, fullname, designation, role, profile_pic_url };
+                    res.locals.staff = staffData;
+                    const token = jwt.sign(
+                      {
+                        id: staff._id,
+                      },
+                      process.env.JWT_SECRET,
+                      {
+                        expiresIn: "30m",
+                      }
+                    );
+                    res.cookie("jwt_token", token, {
+                      httpOnly: true,
+                      secure: true,
+                      maxAge: 30 * 60 * 1000,
+                    });
+                  } else {
+                    res.cookie("jwt_token", "", { maxAge: 1 });
+                    return res.redirect("/staff-login");
+                  }
+                }
+              );
+              next();
+            }
+          }
+        );
+      }
+    }catch(err){
+      req.flash("error", "An error occured, Please try again later");
+      return res.redirect("/staff-login");
+    }
+  },
+
+  adminLevelAuth: (req, res, next) => {
+    try {
+      const token = req.cookies.jwt_token;
+      if (token) {
+        jwt.verify(
+          token,
+          process.env.JWT_SECRET,
+          async (err, decodedToken) => {
+            if (err) {
+              res.locals.staff = null;
+              req.flash("error", "An error occured");
+              res.redirect("/staff-login");
+            } else {
+              await Staff.findOne({
+                _id: decodedToken.id
+              }, (err, foundStaff) => {
                 if (err) {
                   console.log(err);
-                  next(ErrorHandler.serverError());
-                } else if (staff) {
-                  const { id, fullname, designation, profile_pic_url } = staff;
-                  // console.log("OKat here", staff)
-                  const staffData = { id, fullname, designation, profile_pic_url };
-                  // console.log("This is the staff", staffData);
-                  res.locals.staff = staffData;
-                  // console.log("This is locals", res.locals);
-                  const token = jwt.sign(
-                    {
-                      id: staff._id,
-                    },
-                    process.env.JWT_SECRET,
-                    {
-                      expiresIn: "30m",
-                    }
-                  );
-                  res.cookie("jwt_token", token, {
-                    httpOnly: true,
-                    secure: true,
-                    maxAge: 30 * 60 * 1000,
-                  });
-                } else {
+                  req.flash("error", "An error occured, Please try again later");
+                  return res.redirect("back");
+                } else if (!foundStaff) {
                   res.cookie("jwt_token", "", { maxAge: 1 });
+                  req.flash("error", "Invalid token");
                   return res.redirect("/staff-login");
+                } else {
+                  // res.locals.staff_role = foundStaff.role;
+                  console.log(foundStaff);
+                  const role = foundStaff.role;
+                  if (foundStaff.role == "admin") {
+                    next();
+                  } else {
+                    req.flash("error", "You are Not Authorized to access this functionality");
+                    return res.redirect("back");
+                  }
                 }
-              }
-            );
-            next();
-          }
-        }
-      );
+              })
+            }
+          })
+      } else {
+        req.flash("error", "Please login to authenticate yourself");
+        return res.redirect("/staff-login")
+      }
+    } catch (err) {
+      req.flash("error", "An error occured, Please try again later");
+      return res.redirect("/dashboard")
+    }
+  },
+
+  presidentLevelAuth: (req, res, next) => {
+    try {
+      const token = req.cookies.jwt_token;
+      if (token) {
+        jwt.verify(
+          token,
+          process.env.JWT_SECRET,
+          async (err, decodedToken) => {
+            if (err) {
+              res.locals.staff = null;
+              req.flash("error", "An error occured");
+              res.redirect("/staff-login");
+            } else {
+              await Staff.findOne({
+                _id: decodedToken.id
+              }, (err, foundStaff) => {
+                if (err) {
+                  console.log(err);
+                  req.flash("error", "An error occured, Please try again later");
+                  return res.redirect("back");
+                } else if (!foundStaff) {
+                  res.cookie("jwt_token", "", { maxAge: 1 });
+                  req.flash("error", "Invalid token");
+                  return res.redirect("/staff-login");
+                } else {
+                  // res.locals.staff_role = foundStaff.role;
+                  console.log(foundStaff);
+                  const role = foundStaff.role;
+                  if (role == "admin" || role == "president") {
+                    next();
+                  } else {
+                    req.flash("error", "You are Not Authorized to access this functionality");
+                    return res.redirect("/dashboard");
+                  }
+                }
+              })
+            }
+          })
+      } else {
+        req.flash("error", "Please login to authenticate yourself");
+        return res.redirect("/staff-login")
+      }
+    } catch (err) {
+      req.flash("error", "An error occured, Please try again later");
+      return res.redirect("/dashboard")
+    }
+  },
+
+  teamLeaderLevelAuth: (req, res, next) => {
+    try {
+      const token = req.cookies.jwt_token;
+      if (token) {
+        jwt.verify(
+          token,
+          process.env.JWT_SECRET,
+          async (err, decodedToken) => {
+            if (err) {
+              res.locals.staff = null;
+              req.flash("error", "An error occured");
+              res.redirect("/staff-login");
+            } else {
+              await Staff.findOne({
+                _id: decodedToken.id
+              }, (err, foundStaff) => {
+                if (err) {
+                  console.log(err);
+                  req.flash("error", "An error occured, Please try again later");
+                  return res.redirect("back");
+                } else if (!foundStaff) {
+                  res.cookie("jwt_token", "", { maxAge: 1 });
+                  req.flash("error", "Invalid token");
+                  return res.redirect("/staff-login");
+                } else {
+                  // res.locals.staff_role = foundStaff.role;
+                  console.log(foundStaff);
+                  const role = foundStaff.role;
+                  if (role == "admin" || role == "president" || role == "tl") {
+                    next();
+                  } else {
+                    req.flash("error", "You are Not Authorized to access this functionality");
+                    return res.redirect("back");
+                  }
+                }
+              })
+            }
+          })
+      } else {
+        req.flash("error", "Please login to authenticate yourself");
+        return res.redirect("/staff-login")
+      }
+    } catch (err) {
+      req.flash("error", "An error occured, Please try again later");
+      return res.redirect("/dashboard")
+    }
+  },
+
+  memberLevelAuth: (req, res, next) => {
+    try {
+      const token = req.cookies.jwt_token;
+      if (token) {
+        jwt.verify(
+          token,
+          process.env.JWT_SECRET,
+          async (err, decodedToken) => {
+            if (err) {
+              res.locals.staff = null;
+              req.flash("error", "An error occured");
+              res.redirect("/staff-login");
+            } else {
+              await Staff.findOne({
+                _id: decodedToken.id
+              }, (err, foundStaff) => {
+                if (err) {
+                  console.log(err);
+                  req.flash("error", "An error occured, Please try again later");
+                  return res.redirect("back");
+                } else if (!foundStaff) {
+                  res.cookie("jwt_token", "", { maxAge: 1 });
+                  req.flash("error", "Invalid token");
+                  return res.redirect("/staff-login");
+                } else {
+                  // res.locals.staff_role = foundStaff.role;
+                  console.log(foundStaff);
+                  const role = foundStaff.role;
+                  if (role == "admin" || role == "president" || role == "TL" || role == "member") {
+                    next();
+                  } else {
+                    req.flash("error", "You are Not Authorized to access this functionality");
+                    return res.redirect("back");
+                  }
+                }
+              })
+            }
+          })
+      } else {
+        req.flash("error", "Please login to authenticate yourself");
+        return res.redirect("/staff-login")
+      }
+    } catch (err) {
+      req.flash("error", "An error occured, Please try again later");
+      return res.redirect("/dashboard")
     }
   },
 };
